eb8eacdfa07d1dc779ad22bd64a36707c8174f2f
stefanbunde <misc.stefanbunde@gmail.com>
2013-09-29 00:12:32 +0200

    first models und test views

--- openecomaps/settings.py
+++ openecomaps/settings.py
@@ -145,8 +145,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'app_map',
-    'app_logos',
+#    'app_map',
+#    'app_logos',
+    'oem',
 )
 
 # A sample logging configuration. The only tangible logging
--- openecomaps/urls.py
+++ openecomaps/urls.py
@@ -6,18 +6,47 @@ admin.autodiscover()
 
 
 urlpatterns = patterns( '',
-    url( r'^$',           'app_map.views.home' ),
-    url( r'^home$',       'app_map.views.home' ),
+    # ex: /
+    url( r'^$',           'oem.views.home_view' ),
+    # ex: /home/
+    url( r'^home$',       'oem.views.home_view' ),
     
-    url( r'^impressum$',  'views.info_view', {'template_name' : 'impressum.html'} ),
-    url( r'^copyrights$', 'views.info_view', {'template_name' : 'copyrights.html'} ),
-    url( r'^about$',      'views.info_view', {'template_name' : 'about.html'} ),
+    # ex: /login/
+    url( r'^login$',      'oem.views.info_view', {'template_name' : 'login.html'} ),
+    # ex: /about/
+    url( r'^about$',      'oem.views.info_view', {'template_name' : 'about.html'} ),
+    # ex: /copyrights/
+    url( r'^copyrights$', 'oem.views.info_view', {'template_name' : 'copyrights.html'} ),
+    # ex: /impressum/
+    url( r'^impressum$',  'oem.views.info_view', {'template_name' : 'impressum.html'} ),
+    
+    # ex: /search/berlin
+    url( r'^search/(?P<search_term>.*)$',   'oem.views.search_view' ),
+    
+    # ex: /category/fish
+    url( r'^category/(?P<category>.*)$',    'oem.views.category_overview' ),
+    # ex: /category/fish/city/stuttgart
+    url( r'^category/(?P<category>.*)/city/(?P<city>.*)$', 'oem.views.category_city' ),
     
     
+    # ex: /add/poi/...
+    url( r'^add/poi/(?P<data>.*)$',     'oem.views.add_poi_view' ),
+    # ex: /del/poi/...
+    url( r'^del/poi/(?P<data>.*)$',     'oem.views.del_poi_view' ),
+    # ex: /update/poi/...
+    url( r'^update/poi/(?P<data>.*)$',  'oem.views.update_poi_view' ),
+    
+    
+    # ex: 
     url( r'^static/(?P<path>.*)$', 'django.views.static.serve' ),
 
 
+    # ex: 
     url( r'^logo_pages/', include('app_logos.urls') ),
+    # ex: /create_dummy_data/
+    url( r'^create_dummy_data$', 'oem.tests.create_dummy_data_view' ),
+    # ex: /show_dummy_data/
+    url( r'^show_dummy_data$', 'oem.tests.show_dummy_data_view' ),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
