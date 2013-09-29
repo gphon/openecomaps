ad06f6405ba7aea7b1e0739846d58808874c2160
stefanbunde <misc.stefanbunde@gmail.com>
2013-09-29 20:32:53 +0200

    implement fetch_pois view

--- app_logos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns( 'app_logos.views',
-
-    url( r'^(?P<menu>.*)/(?P<tab>.*)$', 'logo_details' ),
-
-)
--- openecomaps/urls.py
+++ openecomaps/urls.py
@@ -7,46 +7,45 @@ admin.autodiscover()
 
 urlpatterns = patterns( '',
     # ex: /
-    url( r'^$',           'oem.views.home_view' ),
+    url( r'^$',           'app_oem.views.home' ),
     # ex: /home/
-    url( r'^home$',       'oem.views.home_view' ),
+    url( r'^home$',       'app_oem.views.home' ),
     
     # ex: /login/
-    url( r'^login$',      'oem.views.info_view', {'template_name' : 'login.html'} ),
+    url( r'^login$',      'app_oem.views.info_view', {'template_name' : 'login.html'} ),
     # ex: /about/
-    url( r'^about$',      'oem.views.info_view', {'template_name' : 'about.html'} ),
+    url( r'^about$',      'app_oem.views.info_view', {'template_name' : 'about.html'} ),
     # ex: /copyrights/
-    url( r'^copyrights$', 'oem.views.info_view', {'template_name' : 'copyrights.html'} ),
+    url( r'^copyrights$', 'app_oem.views.info_view', {'template_name' : 'copyrights.html'} ),
     # ex: /impressum/
-    url( r'^impressum$',  'oem.views.info_view', {'template_name' : 'impressum.html'} ),
+    url( r'^impressum$',  'app_oem.views.info_view', {'template_name' : 'impressum.html'} ),
     
     # ex: /search/berlin
-    url( r'^search/(?P<search_term>.*)$',   'oem.views.search_view' ),
+    url( r'^search/(?P<search_term>.*)$',   'app_oem.views.search_view' ),
     
     # ex: /category/fish
-    url( r'^category/(?P<category>.*)$',    'oem.views.category_overview' ),
+    url( r'^category/(?P<category>.*)$',    'app_oem.views.category_overview' ),
     # ex: /category/fish/city/stuttgart
-    url( r'^category/(?P<category>.*)/city/(?P<city>.*)$', 'oem.views.category_city' ),
+    url( r'^category/(?P<category>.*)/city/(?P<city>.*)$', 'app_oem.views.category_city' ),
     
     
     # ex: /add/poi/...
-    url( r'^add/poi/(?P<data>.*)$',     'oem.views.add_poi_view' ),
+    url( r'^add/poi/(?P<data>.*)$',     'app_oem.views.add_poi' ),
     # ex: /del/poi/...
-    url( r'^del/poi/(?P<data>.*)$',     'oem.views.del_poi_view' ),
+    url( r'^del/poi/(?P<data>.*)$',     'app_oem.views.del_poi' ),
     # ex: /update/poi/...
-    url( r'^update/poi/(?P<data>.*)$',  'oem.views.update_poi_view' ),
+    url( r'^update/poi/(?P<data>.*)$',  'app_oem.views.update_poi_view' ),
     
+    url( r'^fetch/pois/(?P<lon_left>.*)/(?P<lon_right>.*)/(?P<lat_top>.*)/(?P<lat_bottom>.*)$', 'app_oem.views.fetch_pois'),
     
     # ex: 
     url( r'^static/(?P<path>.*)$', 'django.views.static.serve' ),
 
 
-    # ex: 
-    url( r'^logo_pages/', include('app_logos.urls') ),
     # ex: /create_dummy_data/
-    url( r'^create_dummy_data$', 'oem.tests.create_dummy_data_view' ),
+    url( r'^create_dummy_data$', 'app_oem.tests.create_dummy_data_view' ),
     # ex: /show_dummy_data/
-    url( r'^show_dummy_data$', 'oem.tests.show_dummy_data_view' ),
+    url( r'^show_dummy_data$', 'app_oem.tests.show_dummy_data_view' ),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
