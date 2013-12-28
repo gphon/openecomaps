797994ace2b467f36262a0d75a7e9e71c80f0ddd
Amos Buhler <amosbuhler@localhost.(none)>
2013-12-28 16:42:54 +0100

    logo eingebunden, POI hinzufügen realisiert, content css verbessertet

--- static_files/js/function.js
+++ static_files/js/function.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-	var browserhoehe = $(window).height();
-	$('#main_content, #tabs').css({'height': browserhoehe-170});
+	var browserhoehe = $(document).height();
+	$('.karte').css('height',browserhoehe-170);
+	$('#tabs').height($('#main_content').height());
+	
+	// Link in iFrame anzeigen
+	$('.openIframeLink').click(function() {
+		if(!$('.overlay').length){
+			$('body').append('<div class="overlay hidden"><div class="overlayContent"><iframe class="overlayIframe" src="' + $(this).attr('href') + '"></iframe><img src="/static/img/close.png" alt="Schließen" class="closeOverlay"></div></div>');
+		}
+		$('.overlay').fadeIn("fast");
+		$('.overlay').height($(document).height());
+		return false;
+	});
+	$(document.body).on("click",".overlay",function() {
+		$(".overlay").fadeOut("fast");
+	});
+	
 });
