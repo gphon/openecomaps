49bb0b677279eeb0db35cee15f50e213a90d30bd
Stefan Bunde <stefanbunde@gmail.com>
2014-02-19 14:54:47 +0100

    bugfix in home view (remote address not set

--- apps/oem/views/home.py
+++ apps/oem/views/home.py
@@ -24,19 +24,23 @@ def home( request ):
             remote_address = request.META.get('REMOTE_ADDR')
             
             #: use freeGeoIP API to get the current city of the user
-            url = 'http://freegeoip.net/json/' + '86.56.7.45'#+ remote_address
+            url = 'http://freegeoip.net/json/' + remote_address
             
             #: try to get api request and fetch the current city
             #: if timeout do nothing (default value is used)
             try:
                 resp = urllib.request.urlopen( url, timeout=2 )
                 json = eval( resp.read().decode('utf-8') )
-                address = json['city']
-                zoom = 11
+                if json['city']:
+                    address = json['city']
+                    zoom = 11
+                else:
+                    address = 'Germany'
+                    zoom = 6
             except timeout:
                 print( 'socket timed out - URL %s', url )
-                zoom = 6
                 address = 'Germany'
+                zoom = 6
             
             #: get the map data from google maps for the given city
             bounds = fetch_bounds_with_google_api( address )
